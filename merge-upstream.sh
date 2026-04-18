b2436aad1fb2bfa792e51ff956e6bf0e3c5882b2
zdm <zdm@softvisio.net>
2026-04-18 07:59:11 +0300

    chore: update meradata

--- merge-upstream.sh
+++ merge-upstream.sh
@@ -18,15 +18,15 @@ fi
 git switch update-readme
 git merge --no-edit upstream/master
 
-git switch fix/patch-treesitter
+git switch feat/migrate-treesitter
 git merge --no-edit upstream/master
 
 git switch main
 git reset --hard $(git rev-list --max-parents=0 HEAD)
 git merge --no-edit upstream/master
 
-# git merge --no-edit update-readme fix/patch-treesitter
+# git merge --no-edit update-readme feat/migrate-treesitter
 git merge --no-edit update-readme
-git merge --no-edit fix/patch-treesitter
+git merge --no-edit feat/migrate-treesitter
 
 git push --all --force
